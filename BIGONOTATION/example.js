d8aa7ae0a3d8c9df79590853bae43fb148a5ef5a
TabotCharlesBessong <ebezebeatrice@gmail.com>
2022-03-25 12:09:33 +0100

    Innitialised repo

---
+++ BIGONOTATION/example.js
@@ -0,0 +1,32 @@
+
+
+// Linear example
+let n = 100
+for(let i = 0; i < n ; i++){
+  // console.log(i,'\n');
+}
+
+// example for a quadratic cituation 
+
+for(let j = 0 ; j < 15 ; j++){
+  for(let k = 0; k< 15; k++){
+    // console.log(k,"\n");
+  }
+}
+
+
+// example for a cubic situation
+for(let j = 0 ; j < n ; j++){
+  for(let k = 0; k< n; k++){
+    for(let i = 0; i < n ; i++){
+      //  console.log(i,"\n");
+      
+    }
+  }
+}
+
+// example for logarithmic situation
+
+for(let l = 2;l< n ; l = l*2){
+  console.log(l,"\n");
+}
