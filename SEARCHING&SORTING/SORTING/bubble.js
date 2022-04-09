4582c86bce8f42c09ef01341d85f29ea41813292
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-09 12:22:47 +0100

    bubble sort

---
+++ SEARCHING&SORTING/SORTING/bubble.js
@@ -0,0 +1,28 @@
+// This algorithm takes from element 0 to element n-1 and compare one after the other , 
+// The i = 0 element is looped and compared with all the other element 
+
+const swap = (arr,idx1,idx2)=>{
+  let temp = arr[idx1]
+  arr[idx2] = arr[idx2]
+  arr[idx2] = temp
+}
+
+const bubbleMin = (arr)=>{
+  for(let i = 0 , arrLen = arr.length; i < arrLen ; i++){
+    for(let j = 0 ; j< i ; j++){
+      if(arr[i] < arr[j]) swap(arr,i,j)
+    }
+  }
+  return arr
+}
+
+const bubbleMax = (arr)=>{
+  for(let i = 0 , arrLen = arr.length; i < arrLen ; i++){
+    for(let j = 0 ; j< i ; j++){
+      if(arr[i] > arr[j]) swap(arr,i,j)
+    }
+  }
+  return arr
+}
+
+console.log(bubbleMin([6,1,2,3,4,5,8,7]))
